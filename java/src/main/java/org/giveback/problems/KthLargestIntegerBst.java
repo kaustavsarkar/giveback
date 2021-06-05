c6da937a2e16d35ce9e50785d501dca641e34b9d
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2021-06-05 07:12:01 +0530

    Adding solution with Morris' Algorithm.

--- java/src/main/java/org/giveback/problems/KthLargestIntegerBst.java
+++ java/src/main/java/org/giveback/problems/KthLargestIntegerBst.java
@@ -17,10 +17,58 @@ public final class KthLargestIntegerBst {
     private Node<Integer> kthLargestNode;
     private int nodeFromRight;
 
+    /**
+     * Morris' Algorithm helps to traverse through the tree without using an
+     * extra memory. It does uses few extra iterations for that.
+     *
+     * @param root
+     * @param k
+     * @return
+     */
+    public Node<Integer> morrisApproach(Node<Integer> root, int k) {
+        if (root == null) {
+            return null;
+        }
+        var current = root;
+        Node<Integer> predecessor = null;
+        var count = 0;
+
+        while (current != null) {
+            // We have reached the max for a sub-tree.
+            if (current.getRightChild() == null) {
+                count++;
+                current = current.getLeftChild();
+            } else {
+                // If there are elements to the right then we find the left
+                // most child of the right sub tree and assign the current
+                // node to the left child.
+                predecessor = current.getRightChild();
+                while (predecessor.getLeftChild() != null &&
+                        !predecessor.getLeftChild().equals(current)) {
+                    predecessor = predecessor.getLeftChild();
+                }
+                // If the left child to the predecessor is null then we
+                // assign current node to it.
+                if (predecessor.getLeftChild() == null) {
+                    predecessor.setLeftChild(current);
+                    current = current.getRightChild();
+                } else {
+                    predecessor.setLeftChild(null);
+                    count++;
+                    current = current.getLeftChild();
+                }
+            }
+            if (count + 1 == k && current != null) {
+                return current;
+            }
+        }
+        throw new IllegalArgumentException("Tree is smaller than k");
+    }
+
     public Node<Integer> iterativeSolution(Node<Integer> root, int k) {
-        Stack<Node<Integer>> nodeStack = new Stack<>();
-        Node<Integer> current = root;
-        int kthNumber = 0;
+        var nodeStack = new Stack<Node<Integer>>();
+        var current = root;
+        var kthNumber = 0;
 
         while (current != null || !nodeStack.isEmpty()) {
             while (current != null) {
--- java/src/test/java/org/giveback/problems/KthLargestIntegerBstTest.java
+++ java/src/test/java/org/giveback/problems/KthLargestIntegerBstTest.java
@@ -20,8 +20,8 @@ public final class KthLargestIntegerBstTest {
             bst.insert(number);
         }
 
-        KthLargestIntegerBst claz = new KthLargestIntegerBst();
-        Node<Integer> node = claz.recursiveTraversal(bst.getRoot(), 3);
+        var claz = new KthLargestIntegerBst();
+        var node = claz.recursiveTraversal(bst.getRoot(), 3);
 
         assertEquals(node.getKey().intValue(), 27);
     }
@@ -29,13 +29,27 @@ public final class KthLargestIntegerBstTest {
     @Test
     public void iter_find_thirdLargest_success() {
         int[] array = {20, 19, 25, 24, 29, 27, 28, 26};
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        var bst = new BinarySearchTree<Integer>();
         for (Integer number : array) {
             bst.insert(number);
         }
 
-        KthLargestIntegerBst claz = new KthLargestIntegerBst();
-        Node<Integer> node = claz.iterativeSolution(bst.getRoot(), 3);
+        var claz = new KthLargestIntegerBst();
+        var node = claz.iterativeSolution(bst.getRoot(), 3);
+
+        assertEquals(node.getKey().intValue(), 27);
+    }
+
+    @Test
+    public void morris_find_thirdLargest_success() {
+        int[] array = {20, 19, 25, 24, 29, 27, 28, 26};
+        var bst = new BinarySearchTree<Integer>();
+        for (Integer number : array) {
+            bst.insert(number);
+        }
+
+        var claz = new KthLargestIntegerBst();
+        var node = claz.morrisApproach(bst.getRoot(), 3);
 
         assertEquals(node.getKey().intValue(), 27);
     }
@@ -43,13 +57,13 @@ public final class KthLargestIntegerBstTest {
     @Test
     public void kLargerThanNodes_returnsNull() {
         int[] array = {20, 19, 25, 24, 29, 27,};
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        var bst = new BinarySearchTree<Integer>();
         for (Integer number : array) {
             bst.insert(number);
         }
 
-        KthLargestIntegerBst claz = new KthLargestIntegerBst();
-        Node<Integer> node = claz.recursiveTraversal(bst.getRoot(), 7);
+        var claz = new KthLargestIntegerBst();
+        var node = claz.recursiveTraversal(bst.getRoot(), 7);
 
         assertNull(node);
     }
@@ -66,6 +80,18 @@ public final class KthLargestIntegerBstTest {
         claz.iterativeSolution(bst.getRoot(), 7);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void morris_kLargerThanNodes_returnsNull() {
+        int[] array = {20, 19, 25, 24, 29, 27,};
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        for (Integer number : array) {
+            bst.insert(number);
+        }
+
+        var claz = new KthLargestIntegerBst();
+        claz.morrisApproach(bst.getRoot(), 7);
+    }
+
     @Test
     public void returnsNull_nullTree() {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
@@ -78,9 +104,16 @@ public final class KthLargestIntegerBstTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void iter_returnsNull_nullTree() {
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        var bst = new BinarySearchTree<Integer>();
 
-        KthLargestIntegerBst claz = new KthLargestIntegerBst();
+        var claz = new KthLargestIntegerBst();
         claz.iterativeSolution(bst.getRoot(), 3);
+    }
+
+    public void morris_returnsNull_nullTree() {
+        var bst = new BinarySearchTree<Integer>();
+
+        var claz = new KthLargestIntegerBst();
+        claz.morrisApproach(bst.getRoot(), 3);
     }
 }
