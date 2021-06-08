eb697de87e8ba3f1365fca3412e121dc27f5c576
siddhant <siddhantkhare2694@gmail.com>
2021-06-08 22:19:27 +0530

    feat:Add Soln. ConsecutiveLongestSequence & tests

---
+++ cpp/problems/LongestConsecutiveSequence.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Given an unsorted array of integers array, return the length of the longest
+ * consecutive elements sequence
+ */
+
+//Example 1:
+//        Input: array = [100,4,200,1,3,2]
+//        Output: 4
+//        Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4
+
+// Example 2:
+//        Input: array = [0,3,7,2,5,8,4,6,0,1]
+//        Output: 9
+
+// Example 3:
+//        Input: array = [9,63,65,14,10,11,21,22,14,13,15]
+//        Output: 3
+
+class Solution
+{
+public:
+    int longestConsecutive(vector<int> &array)
+    {
+        unordered_set<int> record(array.begin(), array.end());
+        int longestSequence = 1;
+        for (int n : array)
+        {
+            if (record.find(n) == record.end())
+                continue;
+            record.erase(n);
+            int prev = n - 1, next = n + 1;
+            while (record.find(prev) != record.end())
+                record.erase(prev--);
+            while (record.find(next) != record.end())
+                record.erase(next++);
+            longestSequence = max(longestSequence, next - prev - 1);
+        }
+        return longestSequence;
+    }
+};
